98805eb43d3169de74b60f2de32e3efdff35213c
EmreKse <emreqse@gmail.com>
2021-09-22 14:08:17 +0300

    first

---
+++ src/components/authentication/register/RegisterForm.js
@@ -0,0 +1,131 @@
+import * as Yup from 'yup';
+import { useState } from 'react';
+import { Icon } from '@iconify/react';
+import { useFormik, Form, FormikProvider } from 'formik';
+import eyeFill from '@iconify/icons-eva/eye-fill';
+import eyeOffFill from '@iconify/icons-eva/eye-off-fill';
+import { useNavigate } from 'react-router-dom';
+// material
+import { Stack, TextField, IconButton, InputAdornment } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+
+// ----------------------------------------------------------------------
+
+export default function RegisterForm() {
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+
+  const RegisterSchema = Yup.object().shape({
+    firstName: Yup.string().min(2, 'Çok kısa!').max(50, 'Çok uzun!').required('Adınız gereklidir'),
+    lastName: Yup.string()
+      .min(2, 'Çok kısa!')
+      .max(50, 'Çok uzun!')
+      .required('Soyadınız gereklidir'),
+    email: Yup.string()
+      .email('E-postanız geçerli bir formatta olmalıdır')
+      .required('E-posta gereklidir'),
+    password: Yup.string()
+      .required('Parola gereklidir')
+      .min(8, 'Parola en az 8 karakter olmalıdır'),
+    companyName: Yup.string().required('Şirket adınız gereklidir'),
+    companyPhone: Yup.number().required('Şirket telefonunuz gereklidir')
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      companyName: '',
+      companyPhone: ''
+    },
+    validationSchema: RegisterSchema,
+    onSubmit: () => {
+      navigate('/dashboard', { replace: true });
+    }
+  });
+
+  const { errors, touched, handleSubmit, isSubmitting, getFieldProps } = formik;
+
+  return (
+    <FormikProvider value={formik}>
+      <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
+        <Stack spacing={3}>
+          <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2}>
+            <TextField
+              fullWidth
+              label="Ad"
+              {...getFieldProps('firstName')}
+              error={Boolean(touched.firstName && errors.firstName)}
+              helperText={touched.firstName && errors.firstName}
+            />
+
+            <TextField
+              fullWidth
+              label="Soyad"
+              {...getFieldProps('lastName')}
+              error={Boolean(touched.lastName && errors.lastName)}
+              helperText={touched.lastName && errors.lastName}
+            />
+          </Stack>
+
+          <TextField
+            fullWidth
+            autoComplete="username"
+            type="email"
+            label="E-posta Adresi"
+            {...getFieldProps('email')}
+            error={Boolean(touched.email && errors.email)}
+            helperText={touched.email && errors.email}
+          />
+
+          <TextField
+            fullWidth
+            autoComplete="current-password"
+            type={showPassword ? 'text' : 'password'}
+            label="Parola"
+            {...getFieldProps('password')}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton edge="end" onClick={() => setShowPassword((prev) => !prev)}>
+                    <Icon icon={showPassword ? eyeFill : eyeOffFill} />
+                  </IconButton>
+                </InputAdornment>
+              )
+            }}
+            error={Boolean(touched.password && errors.password)}
+            helperText={touched.password && errors.password}
+          />
+
+          <TextField
+            fullWidth
+            label="Şirket Adı"
+            {...getFieldProps('companyName')}
+            error={Boolean(touched.companyName && errors.companyName)}
+            helperText={touched.companyName && errors.companyName}
+          />
+
+          <TextField
+            fullWidth
+            label="Şirket Telefon No"
+            {...getFieldProps('companyPhone')}
+            error={Boolean(touched.companyPhone && errors.companyPhone)}
+            helperText={touched.companyPhone && errors.companyPhone}
+          />
+
+          <LoadingButton
+            fullWidth
+            size="large"
+            type="submit"
+            variant="contained"
+            loading={isSubmitting}
+          >
+            Kayıt Ol
+          </LoadingButton>
+        </Stack>
+      </Form>
+    </FormikProvider>
+  );
+}
---
+++ src/pages/Register.js
@@ -0,0 +1,99 @@
+import { Link as RouterLink } from 'react-router-dom';
+// material
+import { styled } from '@mui/material/styles';
+import { Box, Card, Link, Container, Typography } from '@mui/material';
+// layouts
+import AuthLayout from '../layouts/AuthLayout';
+// components
+import Page from '../components/Page';
+import { MHidden } from '../components/@material-extend';
+import { RegisterForm } from '../components/authentication/register';
+
+// ----------------------------------------------------------------------
+
+const RootStyle = styled(Page)(({ theme }) => ({
+  [theme.breakpoints.up('md')]: {
+    display: 'flex'
+  }
+}));
+
+const SectionStyle = styled(Card)(({ theme }) => ({
+  width: '100%',
+  maxWidth: 464,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  margin: theme.spacing(2, 0, 2, 2)
+}));
+
+const ContentStyle = styled('div')(({ theme }) => ({
+  maxWidth: 480,
+  margin: 'auto',
+  display: 'flex',
+  minHeight: '100vh',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  padding: theme.spacing(12, 0)
+}));
+
+// ----------------------------------------------------------------------
+
+export default function Register() {
+  return (
+    <RootStyle title="Register | Minimal-UI">
+      <AuthLayout>
+        Zaten bir hesabınız mı var? &nbsp;
+        <Link underline="none" variant="subtitle2" component={RouterLink} to="/login">
+          Giriş Yap
+        </Link>
+      </AuthLayout>
+
+      <MHidden width="mdDown">
+        <SectionStyle sx={{ backgroundColor: '#007B55' }}>
+          <Typography variant="h4" sx={{ px: 5, mt: 10, mb: 5, color: 'white' }}>
+            Şirketinizi daha kolay yönetin. Hem de tek bir yazılımla!
+          </Typography>
+          <SectionStyle sx={{ margin: 1, maxWidth: 448 }}>
+            <img alt="register" src="/static/illustrations/business.jpg" />
+          </SectionStyle>
+        </SectionStyle>
+      </MHidden>
+
+      <Container>
+        <ContentStyle>
+          <Box sx={{ mb: 5 }}>
+            <Typography variant="h4" gutterBottom>
+              Ücretsiz deneyin.
+            </Typography>
+            <Typography sx={{ color: 'text.secondary' }}>
+              30 gün ücretsiz deneyin. Kredi kartı bilgilerinizi istemiyoruz.
+            </Typography>
+          </Box>
+
+          <RegisterForm />
+
+          <Typography variant="body2" align="center" sx={{ color: 'text.secondary', mt: 3 }}>
+            Kayıt olarak&nbsp;
+            <Link underline="always" sx={{ color: 'text.primary' }}>
+              Kullanıcı Sözleşmesini
+            </Link>
+            &nbsp;ve&nbsp;
+            <Link underline="always" sx={{ color: 'text.primary' }}>
+              Kişisel Verilerin Korunması ve İşlenmesi Politikasını
+            </Link>
+            &nbsp;kabul ediyorum.
+          </Typography>
+
+          <MHidden width="smUp">
+            <Typography variant="subtitle2" sx={{ mt: 3, textAlign: 'center' }}>
+              Zaten bir hesabınız mı var?&nbsp;
+              <Link to="/login" component={RouterLink}>
+                Giriş Yap
+              </Link>
+            </Typography>
+          </MHidden>
+        </ContentStyle>
+      </Container>
+    </RootStyle>
+  );
+}
